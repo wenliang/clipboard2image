f1bb116b63af6cb2c2532e56e4603b95e14aad49
lwl@mercury <wenliang.lu+mercury@gmail.com>
2017-09-29 19:29:14 -0700

    works for image and text. pygtk rocks

--- clip2image.py
+++ clip2image.py
@@ -5,12 +5,31 @@ pygtk.require('2.0')
 import gtk
 import hashlib
 
+import os
+
+# folder to save
+# TODO: will make this a parameter passed in
+DIR_FIG = '../figure'
+if not os.path.exists(DIR_FIG):
+    os.makedirs(DIR_FIG)
+
 # get the clipboard
 clipboard = gtk.clipboard_get()
+# reference: http://www.pygtk.org/pygtk2reference/class-gtkclipboard.html
+
 image = clipboard.wait_for_image()
 
 if image:
     fn_md5 = hashlib.md5(image.get_pixels()).hexdigest()
-    image.save('%s.png' % (fn_md5), 'png')
+    link_image = '%s/%s.png' % (DIR_FIG, fn_md5)
+    image.save(link_image, 'png')
+    print('![](%s)' % (link_image))
+    exit(0)
+
+text = clipboard.wait_for_text()
+if text:
+    # just return itself
+    print(text)
+    exit(0)
 else:
-    print('No image in clipboard')
+    exit(1)
